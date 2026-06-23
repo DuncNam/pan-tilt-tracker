099200d406e7e644fb041d186ceda5162b86e799
Duncan Nam <namduncan@gmail.com>
2026-06-22 21:04:27 -0500

    Now that tracking is smoother, need to correct boresight adjustment

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -42,7 +42,7 @@ const int US_MAX = 2000;   // pulse width at 180 degrees
 
 // Boresigh adjustment
 const int BORESIGHT_X = 55;   // laser-to-camera offset, horizontal (pixels)
-const int BORESIGHT_Y = 80;   // laser-to-camera offset, vertical (pixels)
+const int BORESIGHT_Y = 50;   // laser-to-camera offset, vertical (pixels)
 
 // Open and configure serial port to Arduino
 int openSerial(const char* port) {
