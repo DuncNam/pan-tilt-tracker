040ec8fc3bc1bd31a2e769fa858ba5bfcd08e8ed
Duncan Nam <namduncan@gmail.com>
2026-06-27 15:46:05 -0500

    Increased KP

--- src/tracker.cpp
+++ src/tracker.cpp
@@ -37,7 +37,7 @@ int lastCy = -1;                // last valid centroid Y
 const int MAX_JUMP = 300;       // reject centroid jumps larger than this many pixels
 
 // Gain - adjustment rate (degrees/pixel)
-const float KP = 0.01f;        // Proportional
+const float KP = 0.02f;        // Proportional
 const float KI = 0.01f;         // Integral
 const float KD = 0.0f;          // Derivative
 
